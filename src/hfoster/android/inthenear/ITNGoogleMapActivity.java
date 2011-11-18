483c354ae4062abb307e23338960fc11d7137d0c
Hartnell Foster <hartnell.foster@googlemail.com>
2011-11-18 21:28:53 +0000

    Layout for Google Maps activities (incomplete).

--- src/hfoster/android/inthenear/ITNGoogleMapActivity.java
+++ src/hfoster/android/inthenear/ITNGoogleMapActivity.java
@@ -1,7 +1,14 @@
 package hfoster.android.inthenear;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class ITNGoogleMapActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.googlemaps);
+	}
 
 }
