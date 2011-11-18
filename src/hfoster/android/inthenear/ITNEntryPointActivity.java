6d0792fbf5dd2be72985f9898da577dfede69987
Hartnell Foster <hartnell.foster@googlemail.com>
2011-11-18 17:56:01 +0000

    Project almost ready for GitHub.

---
+++ src/hfoster/android/inthenear/ITNEntryPointActivity.java
@@ -0,0 +1,13 @@
+package hfoster.android.inthenear;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ITNEntryPointActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ src/hfoster/android/inthenear/ITNGoogleMapActivity.java
@@ -0,0 +1,7 @@
+package hfoster.android.inthenear;
+
+import android.app.Activity;
+
+public class ITNGoogleMapActivity extends Activity {
+
+}
