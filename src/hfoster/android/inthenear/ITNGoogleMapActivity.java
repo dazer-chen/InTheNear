be79ea9b99dfc6a1d5dcaf20fd6e44aa535668f7
Hartnell Foster <hartnell.foster@googlemail.com>
2011-11-19 15:16:20 +0000

    Corrected inheritance for ITNGoogleMapActivity

--- src/hfoster/android/inthenear/ITNGoogleMapActivity.java
+++ src/hfoster/android/inthenear/ITNGoogleMapActivity.java
@@ -1,14 +1,21 @@
 package hfoster.android.inthenear;
 
-import android.app.Activity;
+import com.google.android.maps.MapActivity;
+
 import android.os.Bundle;
 
-public class ITNGoogleMapActivity extends Activity {
+public class ITNGoogleMapActivity extends MapActivity {
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.googlemaps);
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
