f66e48383bd27e459c073fa3566f80194e128211
Hartnell Foster <hartnell.foster@googlemail.com>
2011-11-19 15:15:06 +0000

    Separated Locations ListView into its own Activity and Layout.

---
+++ src/hfoster/android/inthenear/ITNLocationListView.java
@@ -0,0 +1,13 @@
+package hfoster.android.inthenear;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class ITNLocationListView extends ListActivity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.locationlist);
+	}
+
+}
