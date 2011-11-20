865ace19edba88a072d50abdf22267dc02d8c209
Hartnell Foster <hartnell.foster@googlemail.com>
2011-11-20 02:22:12 +0000

    Some late night coding. Getting to grips with ListView.

--- src/hfoster/android/inthenear/ITNEntryPointActivity.java
+++ src/hfoster/android/inthenear/ITNEntryPointActivity.java
@@ -2,6 +2,8 @@ package hfoster.android.inthenear;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ITNEntryPointActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,24 @@ public class ITNEntryPointActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.entrypoint);
+        
+        final Button locListButton = (Button) findViewById(R.id.location_list_button);
+        final Button addLocButton = (Button) findViewById(R.id.add_location_button);
+        locListButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO On-Click action
+				// Call the ITNLocationListView Activity
+			}
+		});
+        addLocButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO On-Click action
+				// Call an Activity where the user can add Locations
+			}
+		});
     }
 }
--- src/hfoster/android/inthenear/ITNLocationListView.java
+++ src/hfoster/android/inthenear/ITNLocationListView.java
@@ -1,13 +1,31 @@
 package hfoster.android.inthenear;
 
 import android.app.ListActivity;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ITNLocationListView extends ListActivity {
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.locationlist);
+		setListAdapter(new ArrayAdapter<Location>(this, R.layout.locationlist, LOCATIONS));
+		
+		ListView lv = getListView();
+		lv.setTextFilterEnabled(true);
+		lv.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				// When clicked, perform some action
+			}
+		});
 	}
+	
+	static final Location[] LOCATIONS = new Location[] { // Can't really use an Array here, as I'll want to add and remove Locations
+		// Some juicy Locations go here
+	};
 
 }
