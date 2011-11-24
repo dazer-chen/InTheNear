6f11436a245e33bc04646ff1abca57dcb73f76be
Hartnell Foster <hartnell.foster@googlemail.com>
2011-11-24 23:43:18 +0000

    2011-11-24 23:43

---
+++ src/hfoster/android/inthenear/ITNLocationService.java
@@ -0,0 +1,15 @@
+package hfoster.android.inthenear;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ITNLocationService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
