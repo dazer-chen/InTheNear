79f1de926a70affbb54831b9e3ad487e69998224
Hartnell Foster <hartnell.foster@googlemail.com>
2011-11-27 20:36:39 +0000

    Still ironing out some bugs in the Google Map Activity.

--- src/hfoster/android/inthenear/ITNGoogleMapActivity.java
+++ src/hfoster/android/inthenear/ITNGoogleMapActivity.java
@@ -5,6 +5,8 @@ import hfoster.android.inthenear.intent.ITNIntent;
 import java.util.List;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,6 +18,7 @@ import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 public class ITNGoogleMapActivity extends MapActivity {
 	
@@ -23,6 +26,8 @@ public class ITNGoogleMapActivity extends MapActivity {
 	static MapView mapView;
 	static MapController mapCtrl;
 	static LocationManager locMgr;
+	static Location loc;
+	static ITNLocationListener locListener;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -32,7 +37,20 @@ public class ITNGoogleMapActivity extends MapActivity {
         mapView.setBuiltInZoomControls(true);
         mapCtrl = mapView.getController();
         locMgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        loc = locMgr.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        locListener = new ITNLocationListener();
+        locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
         List<Overlay> mapOverlays = mapView.getOverlays();
+        Drawable[] mapIcons = new Drawable[11];
+        mapIcons[0] = this.getResources().getDrawable(R.drawable.smiley_happy);
+        for (int i = 1; i < mapIcons.length; i++) {
+        	mapIcons[i] = this.getResources().getDrawable(Integer.parseInt("R.drawable.number_" + i));
+        }
+        ITNItemizedOverlay itemizedOverlay = new ITNItemizedOverlay(mapIcons[0]);
+        // currLoc = new GeoPoint((int) loc.getLatitude(), (int) loc.getLongitude());
+        // OverlayItem userIsHere = new OverlayItem(currLoc, null, null);
+        // itemizedOverlay.addOverlay(userIsHere);
+        // mapOverlays.add(itemizedOverlay);
 	}
 	
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -59,7 +77,7 @@ public class ITNGoogleMapActivity extends MapActivity {
 
 	@Override
 	protected boolean isRouteDisplayed() {
-		// TODO Auto-generated method stub
+		// I don't really know what to do here, so returning false
 		return false;
 	}
 
